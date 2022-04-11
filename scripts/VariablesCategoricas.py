986fc39ff56a2072a9fd1fd9e44735ea25872f34
Mompi19 <52924101+Mompi19@users.noreply.github.com>
2022-04-11 12:24:37 +0200

    asd
    das

---
+++ scripts/VariablesCategoricas.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np 
+from sklearn.lienar_model import LinearRegression
+
+df = pd.read_csv("C://Users//Enrique/Documents/GitHub/python-ml-course/datasets/ecom-expenses/Ecom Expenses.csv")
+print(df.head())
