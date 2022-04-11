b3456e8c83c1f510064d19cbab1200eb06b60b7b
Mompi19 <52924101+Mompi19@users.noreply.github.com>
2022-04-12 00:33:40 +0200

    jhasdfjkh
    sdkljghklfjasd

--- scripts/VariablesCategoricas.py
+++ scripts/VariablesCategoricas.py
@@ -1,6 +1,33 @@
 import pandas as pd
 import numpy as np 
-from sklearn.lienar_model import LinearRegression
+from sklearn.linear_model import LinearRegression
+from sqlalchemy import column
 
-df = pd.read_csv("C://Users//Enrique/Documents/GitHub/python-ml-course/datasets/ecom-expenses/Ecom Expenses.csv")
-print(df.head())
+df = pd.read_csv("E://MachineLearning/python-ml-course/datasets/ecom-expense/Ecom Expense.csv")
+# print(df.head())
+
+dummy_gender = pd.get_dummies(df["Gender"], prefix= 'Gender')
+dummy_city_tier = pd.get_dummies(df['City Tier'], prefix= 'City')
+
+# print(dummy_gender.head())
+# print(dummy_city_tier.head())
+
+
+column_names = df.columns.values.tolist()
+
+df_new = df[column_names].join(dummy_gender)
+
+
+column_names = df_new.columns.values.tolist()
+df_new = df_new[column_names].join(dummy_city_tier)
+print(df_new.head())
+
+features_cols = ['Monthly Income','Transaction Time', 'Gender_Female','Gender_Male','City_Tier 1','City_Tier 2','City_Tier 3','Record','Age ']
+X = df_new[features_cols]
+Y = df_new['Total Spend']
+lm = LinearRegression()
+lm.fit(X,Y)
+print(lm.intercept_)
+print(lm.coef_)
+print(list(zip(features_cols,lm.coef_)))
+print(lm.score(X,Y))
